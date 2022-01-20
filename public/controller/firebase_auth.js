f6f50e6c9b716748fcdb7ee21ab8c5d5515b2ebf
Rohith Krishna <rsubbarayalavajrave@uco.edu>
2022-01-20 03:01:06 -0600

    firebase auth commit

--- public/controller/firebase_auth.js
+++ public/controller/firebase_auth.js
@@ -1,10 +1,24 @@
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-auth.js"
+
 import * as Elements from '../viewpage/elements.js'
+
+const auth = getAuth();
 
 export function addEventListeners() {
 
-    Elements.formSignIn.addEventListener('submit', e => {
+    Elements.formSignIn.addEventListener('submit', async e => {
+        e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password)
+            const user = userCredential.user;
+            console.log(`sign in success:${JSON.stringify(user)}`);
+        } catch (error) {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(`sign in error:${errorCode} | ${errorMessage}`);
+        }
     });
 
 }
