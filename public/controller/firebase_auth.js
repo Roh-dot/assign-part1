f4d0c6b7646527b3ab722239e348ed8b54c1040a
Rohith Krishna <rsubbarayalavajrave@uco.edu>
2022-01-20 20:47:04 -0600

    menu modal changes commit

--- public/controller/firebase_auth.js
+++ public/controller/firebase_auth.js
@@ -1,8 +1,12 @@
-import { getAuth, signInWithEmailAndPassword, signOut } from 'https://www.gstatic.com/firebasejs/9.6.3/firebase-auth.js';
+import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.6.3/firebase-auth.js';
 
 import * as Elements from '../viewpage/elements.js'
 import * as Util from '../viewpage/util.js'
+import * as Constants from '../model/constants.js'
+
 const auth = getAuth();
+
+export let currentUser = null;
 
 export function addEventListeners() {
 
@@ -12,23 +16,50 @@ export function addEventListeners() {
         const password = e.target.password.value;
         try {
             const userCredential = await signInWithEmailAndPassword(auth, email, password)
-            const user = userCredential.user;
-            console.log(`sign in success:${JSON.stringify(user)}`);
+            Elements.modalSignin.hide();
         } catch (error) {
             const errorCode = error.code;
             const errorMessage = error.message;
             Util.info('Sign In Error', JSON.stringify(error), Elements.modalSignin);
-            console.log(`sign in error:${errorCode} | ${errorMessage}`);
+            if (Constants.DEV)
+                console.log(`sign in error:${errorCode} | ${errorMessage}`);
         }
     });
 
     Elements.menuSignOut.addEventListener('click', async () => {
         try {
             await signOut(auth);
-            console.log('sign out success');
         } catch (e) {
-            console.log('sign out error' + e);
+            Util.info('Sign Out Error', JSON.stringify(e));
+            if (Constants.DEV)
+                console.log('sign out error' + e);
         }
     });
 
+    onAuthStateChanged(auth, authStateChangeObserver);
+
+}
+
+function authStateChangeObserver(user) {
+    if (user) {
+        currentUser = user;
+        let elements = document.getElementsByClassName('modal-preauth');
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.display = 'none';
+        }
+        elements = document.getElementsByClassName('modal-postauth');
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.display = 'block';
+        }
+    } else {
+        currentUser = null;
+        let elements = document.getElementsByClassName('modal-preauth');
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.display = 'block';
+        }
+        elements = document.getElementsByClassName('modal-postauth');
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.display = 'none';
+        }
+    }
 }
